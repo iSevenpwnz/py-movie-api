0401eb616d75ba3f689373c68f1e9e663f3c3a8a
Mykhailo Safronov <isevenpwnz@gmail.com>
2025-04-21 20:20:47 +0300

    Refactor Movie model to use PositiveIntegerField for duration and update serializer fields

--- cinema/models.py
+++ cinema/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Movie(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
-    duration = models.IntegerField()
+    duration = models.PositiveIntegerField()
 
     def __str__(self):
         return self.title
--- cinema/serializers.py
+++ cinema/serializers.py
@@ -4,22 +4,16 @@ from cinema.models import Movie
 
 
 class MovieSerializer(serializers.ModelSerializer):
-    id = serializers.IntegerField(read_only=True)
-    title = serializers.CharField(required=True, max_length=100)
-    description = serializers.CharField(required=True)
-    duration = serializers.IntegerField(required=False)
-
     class Meta:
         model = Movie
-        fields = ["id", "title", "description", "duration"]
+        fields = "__all__"
 
     def create(self, validated_data):
         return Movie.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
         instance.title = validated_data.get("title", instance.title)
-        instance.description = validated_data.get(
-            "description", instance.description
-        )
+        instance.description = validated_data.get("description", instance.description)
+        instance.duration = validated_data.get("duration", instance.duration)
         instance.save()
         return instance
